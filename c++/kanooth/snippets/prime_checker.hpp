9f88b0dd283bf9c1b787551766f8a05f7b335e53
Jan Marthedal Rasmussen <jmr@kanooth.com>
2011-11-05 14:00:04 +0100

    dir structure

---
+++ c++/kanooth/snippets/prime_checker.hpp
@@ -0,0 +1,31 @@
+#ifndef _SNIPPET_PRIME_CHECKER_HPP
+#define _SNIPPET_PRIME_CHECKER_HPP
+
+class prime_checker_bounded {
+private:
+  typedef std::vector<bool> Cont;
+  Cont not_prime;
+public:
+  prime_checker_bounded(const typename Cont::size_type n) : not_prime(n+1) {
+    typename Cont::size_type j=1, k;
+    not_prime[0] = not_prime[1] = true;
+    while (true) {
+      do {
+        j++;
+      } while (j <= n && not_prime[j]);
+      if (j > n)
+        break;
+      for (k = j*j; k <= n; k += j)
+        not_prime[k] = true;
+    }
+  }
+  inline bool operator[](const typename Cont::size_type n) const {
+    return !not_prime[n];
+  }
+  inline typename Cont::size_type max() const {
+    return not_prime.size()-1;
+  }
+};
+
+#endif // _SNIPPET_PRIME_CHECKER_HPP
+
